4f6c00371d1c2e2b6286f04bb0e54744fb133fc0
juliana <juliana.ms@live.com>
2022-10-04 01:29:25 -0300

    :pencil2: Fix energy type in Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,8 +1,10 @@
+import Energy from '../Energy';
+
 interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
-  energy?: number;
+  energy?: Energy;
 
   attack(enemy: Fighter): void;
   special?(enemy: Fighter): void;
